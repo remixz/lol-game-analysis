001b93d77d0bc656ddd238e41559a1c88dc1735b
Zach Bruggeman <mail@bruggie.com>
2016-05-16 01:29:12 -0700

    Add Twitter link
    gotta keep up that social media branding baby

--- src/views/app.js
+++ src/views/app.js
@@ -27,8 +27,8 @@ class App extends React.Component {
       <div className='app-view'>
         {this.state.showMessage ? (
           <div className='hello-friends'>
-            <p> Hi! This is an interactive visualization of CLG vs SKT - Game 1 at MSI 2016. Once the data has loaded, a slider will appear, which you can drag to view what was happening in the game at that time. The source code is available <a href='https://github.com/remixz/lol-game-analysis'>on GitHub</a>. Have fun! </p>
-            <p> <em>Pssst... Are you a League team coach? If you are, I'm looking for a team to join as a software analyst. I'd like to work with the analyst team, find out how they're doing their analysis, and create tools like these to make their jobs easier, and their analysis better. If you're interested, <a href='mailto:mail@bruggie.com'>please send me an email</a>. I'd be happy to talk more about some of my ambitious ideas, and how I could be of value to the team. </em> </p>
+            <p> Hi! This is an interactive visualization of CLG vs SKT - Game 1 at MSI 2016. Once the data has loaded, a slider will appear, which you can drag to view what was happening in the game at that time. The source code is available <a href='https://github.com/remixz/lol-game-analysis' target='_blank'>on GitHub</a>. If you liked this, you can <a href='https://twitter.com/zachbruggeman' target='_blank'>follow me on Twitter</a> to find out when I make other cool things. Have fun! </p>
+            <p> <em>Pssst... Are you a League team coach? If you are, I'm looking for a team to join as a data scientist. I'd like to work with the analyst team, find out how they're doing their analysis, and create tools like these to make their jobs easier, and their analysis better. If you're interested, <a href='mailto:mail@bruggie.com'>please send me an email</a>. I'd be happy to talk more about some of my ambitious ideas, and how I could be of value to the team. </em> </p>
             <button className='pure-button pure-button-primary' onClick={this.toggleMessage.bind(this)}> Close this message </button>
           </div>
         ) : null}
