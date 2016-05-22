438a042e43f9353a76b23c52e29301f3d3a094f0
Zach Bruggeman <mail@bruggie.com>
2016-05-22 14:59:04 -0700

    Add link to GitHub repo

--- src/views/app.js
+++ src/views/app.js
@@ -6,7 +6,7 @@ class App extends React.Component {
     return (
       <div className='app-view'>
         <div className='about'>
-          <p> Created by <strong><a href='https://twitter.com/zachbruggeman' target='_blank'>@zachbruggeman</a></strong> | <Link to='/'>About</Link> </p>
+          <p> Created by <strong><a href='https://twitter.com/zachbruggeman' target='_blank'>@zachbruggeman</a></strong> | <Link to='/'>About</Link> | <a href='https://github.com/remixz/lol-game-analysis' target='_blank'>View on GitHub</a> </p>
         </div>
         {this.props.children}
         <p className='fineprint'> League Interactive Timeline isn’t endorsed by Riot Games and doesn’t reflect the views or opinions of Riot Games or anyone officially involved in producing or managing League of Legends. League of Legends and Riot Games are trademarks or registered trademarks of Riot Games, Inc. League of Legends &copy; Riot Games, Inc. </p>
