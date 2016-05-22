a042d08dc986084f2b0cc873d083aea42e003f6a
Zach Bruggeman <mail@bruggie.com>
2016-05-22 14:31:37 -0700

    Add Google Analytics

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import './styles/index.css'
 import React from 'react'
 import { render } from 'react-dom'
 import { Router, Route, IndexRoute, Redirect, browserHistory } from 'react-router'
+import ga from 'react-ga'
 import App from './views/app'
 import Index from './views/index'
 import Match from './views/match'
@@ -11,8 +12,14 @@ window.Config = {
   ddragon: '//ddragon.leagueoflegends.com/cdn/6.10.1'
 }
 
+ga.initialize('UA-46859303-2')
+
+function logPageView () {
+  ga.pageview(window.location.pathname)
+}
+
 render((
-  <Router history={browserHistory}>
+  <Router history={browserHistory} onUpdate={logPageView}>
     <Route path='/' component={App}>
       <IndexRoute component={Index} />
       <Route path='/match/:id' component={Match} />
